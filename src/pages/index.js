1623ad9f0a2c23ef616d8236d66320a627dd3808
João Victor de Lima Martins <jvictor2012@outlook.com>
2021-07-24 14:31:32 -0300

    Material-UI configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
   return (
     <div>
       Clone youtube
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
     </div>
   );
 }
